78173938c4b4541082d13e7238be9f0bd729afd8
kyleschaeffer <kyle@spirito.us>
2018-10-16 16:19:31 -0400

    Install SPFx

---
+++ src/webparts/meter/MeterWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'MeterWebPartStrings';
+import Meter from './components/Meter';
+import { IMeterProps } from './components/IMeterProps';
+
+export interface IMeterWebPartProps {
+  description: string;
+}
+
+export default class MeterWebPart extends BaseClientSideWebPart<IMeterWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IMeterProps > = React.createElement(
+      Meter,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
